71628783d582944b738373bf40e0b80e0b14ebc5
svhl <145035644+svhl@users.noreply.github.com>
2023-09-15 19:45:22 +0530

    improved naming & declaration

--- ds-p1-14-09-23.c
+++ ds-p1-14-09-23.c
@@ -8,18 +8,18 @@ struct Student
 {
 	int rno;
 	char name[50];
-	float m[5];
+	float marks[5];
 	float total;
 };
 
 void main()
 {
-	int n;
+	int n, i, j;
 	printf("Enter no. of students:\n");
 	scanf("%d", &n);
 	struct Student s[n];
 
-	for(int i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
 		printf("\nPerson %d:\n\n", i+1);
 		printf("Enter roll no.:\n");
@@ -28,30 +28,30 @@ void main()
 		scanf("%s", s[i].name);
 		s[i].total = 0;
 
-		for(int j = 0; j < 5; j++)
+		for(j = 0; j < 5; j++)
 		{
 			printf("Enter marks of subject %d:\n", j+1);
-			scanf("%f", &s[i].m[j]);
-			s[i].total += s[i].m[j];
+			scanf("%f", &s[i].marks[j]);
+			s[i].total += s[i].marks[j];
 		}
 	}
 
-	for(int i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
 		printf("\nPerson %d:\n\n", i+1);
 		printf("Roll no.: %d\n", s[i].rno);
 		printf("Name: %s\n", s[i].name);
 
-		for(int j = 0; j < 5; j++)
+		for(j = 0; j < 5; j++)
 		{
 			printf("Marks of subject %d:\n", j+1);
-			printf("%f\n", s[i].m[j]);
+			printf("%f\n", s[i].marks[j]);
 		}
 	}
 
-	for(int i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
-		for(int j = 0; j < n-1-i; j++)
+		for(j = 0; j < n-1-i; j++)
 		{
 			if(s[j].total < s[j+1].total)
 			{
@@ -61,7 +61,8 @@ void main()
 			}
 		}
 	}
-	for(int i = 0; i < n; i++)
+	
+	for(i = 0; i < n; i++)
 	{
 		printf("\nRank %d\n", i+1);
 		printf("Name: %s\n", s[i].name);
---
+++ ds-p2-14-09-23.c
@@ -0,0 +1,32 @@
+//write a program using pointers to search an element in an array using linear search
+
+#include <stdio.h>
+
+void main()
+{
+	int array[20], n, search, *pointer, flag = 0, i;
+	printf("Enter no. of elements:\n");
+	scanf("%d", &n);
+
+	for(i = 0; i < n; i++)
+	{
+		printf("Enter element %d:\n", i+1);
+		scanf("%d", &array[i]);
+	}
+
+	printf("Enter the element to search:");
+	scanf("%d", &search);
+	pointer = array;
+
+	for(i = 0; i < n; i++)
+	{
+		if ( *(pointer+i) == search)
+		{
+			printf("%d found at position %d\n", search, i);
+			flag = 1;
+		}
+	}
+
+	if(flag == 0)
+		printf("%d not found\n", search);
+}
