acfcdc6e46ab8f47536ce62801a4135c8496ee30
svhl <145035644+svhl@users.noreply.github.com>
2023-09-21 22:07:10 +0530

    improved DS prog 1

--- ds-p1-21-09-23.c
+++ ds-p1-21-09-23.c
@@ -1,3 +1,5 @@
+// write a menu driven c program to implement polynomials using arrays & perform polynomial addition
+
 #include <stdio.h>
 
 void main()
@@ -14,7 +16,7 @@ void main()
 		
 		if(ch == 1)
 		{
-			printf("Enter no. of elements in polynomial 1:\n");
+			printf("\nEnter no. of elements in polynomial 1:\n");
 			scanf("%d", &m);
 			a[0] = m;
 			
@@ -61,24 +63,24 @@ void main()
 						r += 2;
 					}
 					
-					p+=2;
-					q+=2;
+					p += 2;
+					q += 2;
 				}
 				
 				else if(a[p] < b[q])
 				{
 					c[r+1] = b[q+1];
 					c[r] = b[q];
-					r+=2;
-					q+=2;
+					r += 2;
+					q += 2;
 				}
 				
 				else
 				{
 					c[r+1] = a[p+1];
 					c[r] = a[p];
-					r+=2;
-					p+=2;
+					r += 2;
+					p += 2;
 				}
 			}
 			
@@ -86,25 +88,35 @@ void main()
 			{
 				c[r] = a[p];
 				c[r+1] = a[p+1];
-				r+=2;
-				p+=2;
+				r += 2;
+				p += 2;
 			}
 			
 			while(q <= 2*n)
 			{
 				c[r] = b[q];
 				c[r+1] = b[q+1];
-				r+=2;
-				q+=2;
+				r += 2;
+				q += 2;
 			}
 			
-			c[0]=(r+1)/2;
+			c[0] = r/2;
 			printf("\nAdded\n");
 		}
 		
 		else if(ch == 3)
 		{
-			printf("\nSum:\n");
+			printf("\nPolynomial 1:\n");
+
+			for(i = 0; i <= m*2; i++)
+				printf("%d\t", a[i]);
+
+			printf("\n\nPolynomial 2:\n");
+
+			for(i = 0; i <= n*2; i++)
+				printf("%d\t", b[i]);
+
+			printf("\n\nSum:\n");
 			
 			for(i = 0; i < r; i++)
 				printf("%d\t", c[i]);
