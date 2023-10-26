3c9c6c865fb2757b827493a8b4aa7cf128288450
svhl <145035644+svhl@users.noreply.github.com>
2023-10-26 13:54:04 +0530

    completed DS prog 8

--- DS/week-4/p8-19-10-23.c
+++ DS/week-4/p8-19-10-23.c
@@ -1,3 +1,12 @@
+// write a menu-driven program to implement the following operations on a singly linked list
+// insert at beginning
+// insert at end
+// insert after a specified node
+// delete from beginning
+// delete from end
+// delete a specified node
+// display
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,7 +16,29 @@ struct node
 	struct node *next;
 };
 
-struct node *p, *head, *tail, *ptr;
+struct node *p, *head, *tail, *ptr, *prev;
+int item;
+
+void insertatbegin()
+{
+	p = (struct node*)malloc(sizeof(struct node));
+	printf("\nEnter the data:\n");
+	scanf("%d", &p->data);
+	printf("\n");
+	p->next = NULL;
+
+	if(head == NULL)
+	{
+		head = p;
+		tail = p;
+	}
+
+	else
+	{
+		p->next = head;
+		head = p;
+	}
+}
 
 void insertatend()
 {
@@ -27,6 +58,87 @@ void insertatend()
 	{
 		tail->next = p;
 		tail = p;
+	}
+}
+
+void insertafter()
+{
+	p = (struct node*)malloc(sizeof(struct node));
+	printf("\nEnter the data:\n");
+	scanf("%d", &p->data);
+	p->next = NULL;
+	ptr = head;
+	printf("Enter node after which new node is to be inserted:\n");
+	scanf("%d", &item);
+	printf("\n");
+	
+	while(ptr->data != item)
+	{
+		ptr = ptr->next;
+	}
+	
+	p->next = ptr->next;
+	ptr->next = p;
+}
+
+void delfrombegin()
+{
+	if(head == NULL)
+	{
+		printf("\nList is empty\n\n");
+	}
+	
+	else
+	{
+		ptr = head;
+		head = ptr->next;
+		printf("\n");
+	}
+}
+
+void delfromend()
+{
+	if(head == NULL)
+	{
+		printf("\nList is empty\n\n");
+	}
+	
+	else
+	{
+		ptr = head;
+		
+		while(ptr->next != NULL)
+		{
+			prev = ptr;
+			ptr = ptr->next;
+		}
+		
+		prev->next = NULL;
+		tail = prev;
+		printf("\n");
+	}
+}
+
+void delafter()
+{
+	if(head == NULL)
+	{
+		printf("\nList is empty\n\n");
+	}
+	
+	else
+	{
+		printf("Enter node to delete:\n");
+		scanf("%d", &item);
+		ptr = head;
+		
+		while(ptr->data != item)
+		{
+			prev = ptr;
+			ptr = ptr->next;
+		}
+		
+		prev->next = ptr->next;
 	}
 }
 
@@ -52,18 +164,38 @@ void main()
 	while(1)
 	{
 		printf("Menu\n");
-		printf("1. Insert at end\n");
-		printf("2. Dsiplay\n");
+		printf("1. Insert at beginning\n");
+		printf("2. Insert at end\n");
+		printf("3. Insert after a specified node\n");
+		printf("4. Delete from beginning\n");
+		printf("5. Delete from end\n");
+		printf("6. Delete a specified node\n");
+		printf("7. Display\n");
 		printf("Any other value to exit\n");
 		printf("Enter choice:\n");
 		scanf("%d", &ch);
 
 		switch(ch)
 		{
-			case 1: insertatend();
+			case 1: insertatbegin();
+				break;
+			
+			case 2: insertatend();
+				break;
+			
+			case 3: insertafter();
+				break;
+			
+			case 4: delfrombegin();
+				break;
+			
+			case 5: delfromend();
+				break;
+			
+			case 6: delafter();
 				break;
 
-			case 2: display();
+			case 7: display();
 				break;
 
 			default: return;
