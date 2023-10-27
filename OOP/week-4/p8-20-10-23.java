ef89988fb044ee15fea4d80ac1c6f39fe03ccd35
svhl <145035644+svhl@users.noreply.github.com>
2023-10-27 10:04:57 +0530

    completed OOP prog 8

--- OOP/week-4/p8-20-10-23.java
+++ OOP/week-4/p8-20-10-23.java
@@ -28,28 +28,11 @@ class Employee
 
 class Manager extends Employee
 {
-	int teamsize;
-
-	Manager(String name, String role, int id, int teamsize)
-	{
-		super(name, role, id);
-		this.teamsize = teamsize;
-	}
-
-	void display()
-	{
-		super.display();
-		System.out.println("Team size: " + teamsize);
-	}
-}
-
-class SeniorManager extends Manager
-{
 	String dept;
 
-	SeniorManager(String name, String role, int id, int teamsize, String dept)
+	Manager(String name, String role, int id, String dept)
 	{
-		super(name, role, id, teamsize);
+		super(name, role, id);
 		this.dept = dept;
 	}
 
@@ -60,13 +43,47 @@ class SeniorManager extends Manager
 	}
 }
 
-class JuniorManager extends Manager
+class SeniorManager extends Manager
+{
+	int tsize;
+
+	SeniorManager(String name, String role, int id, String dept, int tsize)
+	{
+		super(name, role, id, dept);
+		this.tsize = tsize;
+	}
+
+	void display()
+	{
+		super.display();
+		System.out.println("Team size: " + tsize);
+	}
+}
+
+class Developer extends Employee
+{
+	String language;
+
+	Developer(String name, String role, int id, String language)
+	{
+		super(name, role, id);
+		this.language = language;
+	}
+
+	void display()
+	{
+		super.display();
+		System.out.println("Language: " + language);
+	}
+}
+
+class JuniorDeveloper extends Developer
 {
 	String mentor;
 
-	JuniorManager(String name, String role, int id, int teamsize, String mentor)
+	JuniorDeveloper(String name, String role, int id, String language, String mentor)
 	{
-		super(name, role, id, teamsize);
+		super(name, role, id, language);
 		this.mentor = mentor;
 	}
 
@@ -77,44 +94,83 @@ class JuniorManager extends Manager
 	}
 }
 
+class Intern extends Employee
+{
+	String cname;
+
+	Intern(String name, String role, int id, String cname)
+	{
+		super(name, role, id);
+		this.cname = cname;
+	}
+
+	void display()
+	{
+		super.display();
+		System.out.println("College: " + cname);
+	}
+}
+
 class EmployeeDemo
 {
 	public static void main(String[] args)
 	{
-		String name, role, dept, mentor;
-		int id, teamsize;
+		String name, role, dept, language, mentor, cname;
+		int id, tsize;
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Case 1: Senior Manager\n");
-		System.out.println("Enter name:");
+		System.out.println("Menu");
+		System.out.println("1. Senior manager");
+		System.out.println("2. Junior developer");
+		System.out.println("3. Intern");
+		System.out.println("Any other value to exit");
+		System.out.println("Enter choice");
+		int ch = sc.nextInt();
+		
+		if(ch < 1 || ch > 3)
+		{
+			return;
+		}
+		
+		sc.nextLine();
+		System.out.println("\nEnter name:");
 		name = sc.nextLine();
 		System.out.println("Enter role:");
 		role = sc.nextLine();
 		System.out.println("Enter ID:");
 		id = sc.nextInt();
-		System.out.println("Enter team size:");
-		teamsize = sc.nextInt();
-		System.out.println("Enter department:");
-		sc.nextLine();
-		dept = sc.nextLine();
+		
+		if(ch == 1)
+		{
+			System.out.println("Enter team size:");
+			tsize = sc.nextInt();
+			System.out.println("Enter department:");
+			sc.nextLine();
+			dept = sc.nextLine();
 
-		SeniorManager sm1 = new SeniorManager(name, role, id, teamsize, dept);
-		sm1.display();
+			SeniorManager sm1 = new SeniorManager(name, role, id, dept, tsize);
+			sm1.display();
+		}
+		
+		else if(ch == 2)
+		{
+			sc.nextLine();
+			System.out.println("Enter language:");
+			language = sc.nextLine();
+			System.out.println("Enter mentor:");
+			mentor = sc.nextLine();
 
-		System.out.println("\nCase 2: Junior Manager\n");
-		System.out.println("Enter name:");
-		name = sc.nextLine();
-		System.out.println("Enter role:");
-		role = sc.nextLine();
-		System.out.println("Enter ID:");
-		id = sc.nextInt();
-		System.out.println("Enter team size:");
-		teamsize = sc.nextInt();
-		System.out.println("Enter mentor:");
-		sc.nextLine();
-		mentor = sc.nextLine();
+			JuniorDeveloper jd1 = new JuniorDeveloper(name, role, id, language, mentor);
+			jd1.display();
+		}
+		
+		else if(ch == 3)
+		{
+			sc.nextLine();
+			System.out.println("Enter college:");
+			cname = sc.nextLine();
 
-		JuniorManager jm1 = new JuniorManager(name, role, id, teamsize, mentor);
-		jm1.display();
+			Intern i1 = new Intern(name, role, id, cname);
+			i1.display();
+		}
 	}
 }
