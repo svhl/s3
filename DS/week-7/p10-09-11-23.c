3ffcc3f89bf4682fd9ce2bb172fa4f3501e29fcd
svhl <145035644+svhl@users.noreply.github.com>
2023-11-09 15:31:28 +0530

    fixes to DS prog 10

--- DS/week-7/p10-09-11-23.c
+++ DS/week-7/p10-09-11-23.c
@@ -46,7 +46,7 @@ void pop()
 	}
 }
 
-void displaystack()
+void display()
 {
 	if(top == NULL)
 	{
@@ -66,9 +66,28 @@ void displaystack()
 
 		printf("\n\n");
 	}
+	
+	if(front == NULL)
+	{
+		printf("\nQueue is empty\n\n");
+	}
+
+	else
+	{
+		ptr = front;
+		printf("\nQueue:\n");
+
+		while(ptr != NULL)
+		{
+			printf("%d\t", ptr->data);
+			ptr = ptr->next;
+		}
+
+		printf("\n\n");
+	}
 }
 
-void enqueuelinear()
+void enqueue()
 {
 	p = (struct node*)malloc(sizeof(struct node));
 	printf("\nEnter the data:\n");
@@ -89,7 +108,7 @@ void enqueuelinear()
 	}
 }
 
-void dequeuelinear()
+void dequeue()
 {
 	if(front == NULL)
 	{
@@ -109,182 +128,45 @@ void dequeuelinear()
 	}
 
 	printf("\n");
-}
-
-void displaylinear()
-{
-	if(front == NULL)
-	{
-		printf("\nLinear queue is empty\n");
-	}
-
-	else
-	{
-		ptr = front;
-		printf("\nLinear queue:\n");
-		
-		while(ptr != NULL)
-		{
-			printf("%d\t", ptr->data);
-			ptr = ptr->next;
-		}
-	}
-
-	printf("\n\n");
-}
-
-void enqueuecircular()
-{
-	p = (struct node*)malloc(sizeof(struct node));
-	printf("\nEnter the data:\n");
-	scanf("%d", &p->data);
-	printf("\n");
-	p->next = NULL;
-
-	if(front == NULL)
-	{
-		front = p;
-		rear = p;
-		rear->next = p;
-	}
-
-	else
-	{
-		rear->next = p;
-		p->next = front;
-		rear = p;
-	}
-}
-
-void dequeuecircular()
-{
-	if(front == NULL)
-	{
-		printf("\nCircular queue is empty\n");
-	}
-
-	else if(front == rear)
-	{
-		front = NULL;
-		rear = NULL;
-	}
-
-	else
-	{
-		ptr = front;
-		front = ptr->next;
-		rear->next = front;
-	}
-
-	printf("\n");
-}
-
-void displaycircular()
-{
-	if(front == NULL)
-	{
-		printf("\nCircular queue is empty\n");
-	}
-
-	else
-	{
-		ptr = front;
-		printf("\nCircular queue:\n");
-		
-		while(ptr != NULL)
-		{
-			printf("%d\t", ptr->data);
-
-			if(ptr->next == front)
-			{
-				break;
-			}
-
-			ptr = ptr->next;
-		}
-	}
-
-	printf("\n\n");
 }
 
 void main()
 {
-	int op, ch;
-	printf("Menu\n");
-	printf("1. Stack\n");
-	printf("2. Linear queue\n");
-	printf("3. Circular queue\n");
-	printf("Enter choice:\n");
-	scanf("%d", &op);
-	printf("\n");
-
-	if(op == 1)
+	top = NULL;
+	front = NULL;
+	rear = NULL;
+	int ch;
+	
+	while(1)
 	{
-		top = NULL;
-
-		while(1)
-		{
-			printf("Menu\n");
-			printf("1. Push\n");
-			printf("2. Pop\n");
-			printf("3. Display\n");
-			printf("Any other value to exit\n");
-			printf("Enter choice:\n");
-			scanf("%d", &ch);
-
-			switch(ch)
-			{
-				case 1: push();
-					break;
-
-				case 2: pop();
-					break;
-
-				case 3: displaystack();
-					break;
-
-				default: return;
-			}
-		}
-	}
-
-	else if(op == 2 || op == 3)
-	{
-		front = NULL;
-		rear = NULL;
+		printf("Menu\n");
+		printf("1. Push\n");
+		printf("2. Pop\n");
+		printf("3. Enqueue\n");
+		printf("4. Dequeue\n");
+		printf("5. Display\n");
+		printf("Any other value to exit\n");
+		printf("Enter choice:\n");
+		scanf("%d", &ch);
 		
-		while(1)
+		switch(ch)
 		{
-			printf("Menu\n");
-			printf("1. Enqueue\n");
-			printf("2. Dequeue\n");
-			printf("3. Display\n");
-			printf("Any other value to exit\n");
-			printf("Enter choice:\n");
-			scanf("%d", &ch);
-
-			switch(ch)
-			{
-				case 1: if(op == 2)
-						enqueuelinear();
-					else
-						enqueuecircular();
-					break;
-
-				case 2: if(op == 2)
-						dequeuelinear();
-					else
-						dequeuecircular();
-					break;
-
-				case 3: if(op == 2)
-						displaylinear();
-					else
-						displaycircular();
-					break;
-
-				default: return;
-			}
+			case 1: push();
+				break;
+				
+			case 2: pop();
+				break;
+				
+			case 3: enqueue();
+				break;
+				
+			case 4: dequeue();
+				break;
+				
+			case 5: display();
+				break;
+				
+			default: return;
 		}
 	}
 }
