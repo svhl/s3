9b99c2cd51c3d806e4b932b12ff6ba64d0ec1c1e
svhl <145035644+svhl@users.noreply.github.com>
2023-11-24 11:02:04 +0530

    Update p13-10-11-23.java

--- OOP/week-7/p13-10-11-23.java
+++ OOP/week-7/p13-10-11-23.java
@@ -15,10 +15,10 @@ class atm
 		String ano1 = "", ano2;
 		int ch;
 		double bal1 = 0, bal2, amt;
-
+		Scanner sc = new Scanner(System.in);
+		
 		try
 		{
-			Scanner sc = new Scanner(System.in);
 			System.out.println("State Bank of India");
 			System.out.println("Enter account no.:");
 			ano1 = sc.next();
@@ -31,7 +31,7 @@ class atm
 				f1r.close();
 			}
 			
-			catch(FileNotFoundException e)
+			catch(IOException e)
 			{
 				System.out.println("\nAccount doesn't exist\n" + e);
 				FileWriter f1w = new FileWriter(ano1);
@@ -41,96 +41,107 @@ class atm
 				f1w.close();
 				return;
 			}
-			
-			System.out.println("\n1. Withdraw");
-			System.out.println("2. Deposit");
-			System.out.println("3. Transfer");
-			System.out.println("4. Display balance");
-			System.out.println("Any other value to exit");
-			System.out.println("Enter choice:");
-			ch = sc.nextInt();
-			
-			if(ch == 1)
-			{
-				System.out.println("\nEnter amount to withdraw:");
-				amt = sc.nextDouble();
-				
-				if(amt > bal1)
-				{
-					throw new ArithmeticException("\nInsufficient balance");
-				}
-				
-				bal1 = bal1 - amt;
-			}
-			
-			else if(ch == 2)
-			{
-				System.out.println("\nEnter amount to deposit:");
-				amt = sc.nextDouble();
-				bal1 = bal1 + amt;
-			}
-			
-			else if(ch == 3)
-			{
-				System.out.println("\nEnter recipient's account no.:");
-				ano2 = sc.next();
-				FileReader f2r = new FileReader(ano2);
-				Scanner s2 = new Scanner(f2r);
-				bal2 = s2.nextDouble();
-				f2r.close();
-				System.out.println("Enter amount to transfer:");
-				amt = sc.nextDouble();
-				
-				if(amt > bal1)
-				{
-					throw new ArithmeticException("\nInsufficient balance");
-				}
-
-				bal1 = bal1 - amt;
-				amt = amt + bal2;
-				FileWriter f2w = new FileWriter(ano2);
-				f2w.write(Double.toString(amt));
-				f2w.close();
-			}
-
-			else if(ch == 4)
-			{
-			}
-			
-			else
-			{
-				System.out.println("\nWrong choise");
-			}
-		}
-
-		catch(ArithmeticException e)
-		{
-			System.out.println("\n" + e);
 		}
 		
-		catch(FileNotFoundException e)
-		{
-			System.out.println("\nAccount doesn't exist\n" + e);
-		}
-
 		catch(Exception e)
 		{
-			System.out.println("\n" + e);
+			System.out.println(e);
 		}
 		
-		finally
+		while(true)
 		{
 			try
+			{				
+				System.out.println("\n1. Withdraw");
+				System.out.println("2. Deposit");
+				System.out.println("3. Transfer");
+				System.out.println("4. Display balance");
+				System.out.println("Any other value to exit");
+				System.out.println("Enter choice:");
+				ch = sc.nextInt();
+				
+				if(ch == 1)
+				{
+					System.out.println("\nEnter amount to withdraw:");
+					amt = sc.nextDouble();
+					
+					if(amt > bal1)
+					{
+						throw new ArithmeticException("\nInsufficient balance");
+					}
+					
+					bal1 = bal1 - amt;
+				}
+				
+				else if(ch == 2)
+				{
+					System.out.println("\nEnter amount to deposit:");
+					amt = sc.nextDouble();
+					bal1 = bal1 + amt;
+				}
+				
+				else if(ch == 3)
+				{
+					System.out.println("\nEnter recipient's account no.:");
+					ano2 = sc.next();
+					FileReader f2r = new FileReader(ano2);
+					Scanner s2 = new Scanner(f2r);
+					bal2 = s2.nextDouble();
+					f2r.close();
+					System.out.println("Enter amount to transfer:");
+					amt = sc.nextDouble();
+					
+					if(amt > bal1)
+					{
+						throw new ArithmeticException("\nInsufficient balance");
+					}
+
+					bal1 = bal1 - amt;
+					amt = amt + bal2;
+					FileWriter f2w = new FileWriter(ano2);
+					f2w.write(Double.toString(amt));
+					f2w.close();
+				}
+
+				else if(ch == 4)
+				{
+				}
+				
+				else
+				{
+					return;
+				}
+			}
+
+			catch(ArithmeticException e)
 			{
-				FileWriter f1w = new FileWriter(ano1);
-				System.out.println("\nBalance: " + bal1);
-				f1w.write(Double.toString(bal1));
-				f1w.close();
+				System.out.println("\n" + e);
+			}
+			
+			catch(FileNotFoundException e)
+			{
+				System.out.println("\nAccount doesn't exist\n" + e);
 			}
 
 			catch(Exception e)
 			{
 				System.out.println("\n" + e);
+			}
+			
+			finally
+			{
+				try
+				{
+					FileWriter f1w = new FileWriter(ano1);
+					System.out.println("\nBalance: " + bal1);
+					f1w.write(Double.toString(bal1));
+					f1w.close();
+				}
+
+				catch(Exception e)
+				{
+					System.out.println("\n" + e);
+				}
 			}
 		}
 	}
